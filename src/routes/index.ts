0d6c902e9c42ff480ee67a12768d4d8e9715d14a
Ejiawoko George <ejiawokogeorge@gmail.com>
2023-11-09 12:36:58 +0100

    added omitted routing for user and auth routes.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import paymentRouter from './payment';
+import userRouter from './user';
+import authRouter from './auth';
 
 const router = express.Router();
 
 router.use('/payments', paymentRouter);
+router.use('/auth', authRouter);
+router.use('/user', userRouter);
 
 
 export default router;
