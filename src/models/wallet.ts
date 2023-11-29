c25287ccfad08f9bcacb7bbfa1490faffa8bb89f
Ejiawoko George <ejiawokogeorge@gmail.com>
2023-11-29 06:06:06 +0100

    modified wallet model

--- src/models/wallet.ts
+++ src/models/wallet.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const {Schema, model} = mongoose;
 
 const walletSchema = new Schema({
-    owner: {type: Schema.Types.ObjectId, ref: 'Users', unique: true},
+    owner: {type: Schema.Types.ObjectId, ref: 'Users', unique: true, required: true},
     balance: {type: Number, default: 0},
     currency: {type: String, default: 'NGN'},
     accountNumber: {type: String, length: 10},
